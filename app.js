614fc333251a68019409615a3cfdb6b4a122401f
Airen <airenilia@gmail.com>
2023-02-09 04:14:25 +0700

    Added embed edit command

--- app.js
+++ app.js
@@ -42,7 +42,7 @@ client.on(Events.InteractionCreate, async interaction => {
 		await command.execute(interaction);
 	} catch (error) {
 		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+		await interaction.reply({ content: `Error! ${error}`, ephemeral: true });
 	}
 });
 
--- commands/embed.js
+++ commands/embed.js
@@ -3,22 +3,48 @@ import { PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
 	.setName('embed')
-	.setDescription('Make a pretty embed from JSON file.')
-	.addStringOption(option => option
-		.setName("link")
-		.setDescription("Link to JSON file with embed")
-		.setRequired(true))
+	.setDescription('Handle discord embedded messages.')
+	.addSubcommand(subcommand => subcommand
+		.setName('create')
+		.setDescription('Create new message with embed from JSON')
+		.addStringOption(option => option
+			.setName('link')
+			.setDescription('Link to embed JSON')
+			.setRequired(true)))
+	.addSubcommand(subcommand => subcommand
+		.setName('edit')
+		.setDescription('Replace message with embed from JSON')
+		.addStringOption(option => option
+			.setName('id')
+			.setDescription('Message ID')
+			.setRequired(true))
+		.addStringOption(option => option
+			.setName('link')
+			.setDescription('Link to embed JSON')
+			.setRequired(true)))
 	.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
 
 export async function execute(interaction) {
-	const content = await (await fetch(interaction.options.getString('link'))).json()
-		.catch(console.error);
+	const content = await (await fetch(interaction.options.getString('link'))
+		.catch(console.error)).json()
+			.catch(console.error);
 	
 	interaction.deferReply({ ephemeral: true })
 		.catch(console.error);
 
-	interaction.channel.send(content)
-		.catch(console.error);
+	const subcommand = interaction.options.getSubcommand();
+	switch(subcommand) {
+		case 'create': {
+			interaction.channel.send(content)
+				.catch(console.error);
+		}
+		case 'edit': {
+			const message = await interaction.channel.messages.fetch(interaction.options.getString('id'))
+				.catch(console.error);
+			message.edit(content)
+				.catch(console.error);
+		}
+	}
 
 	interaction.deleteReply()
 		.catch(console.error);
